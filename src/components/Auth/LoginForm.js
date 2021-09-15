bf016292e1f942569e45805f55fe259a809d1e6e
Agustin Navarro Galdon <xAgustin93@gmail.com>
2021-09-15 23:12:33 +0200

    Generando el formulairo de login

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,10 +1,46 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  Keyboard,
+} from "react-native";
 
 export default function LoginForm() {
   return (
     <View>
-      <Text>LoginForm....</Text>
+      <Text style={styles.title}>Iniciar sesión</Text>
+      <TextInput
+        placeholder="Nombre de usuario"
+        style={styles.input}
+        autoCapitalize="none"
+      />
+      <TextInput
+        placeholder="Contraseña"
+        style={styles.input}
+        autoCapitalize="none"
+        secureTextEntry={true}
+      />
+      <Button title="Entrar" onPress={() => console.log("Entrando...")} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: "center",
+    fontSize: 28,
+    fontWeight: "bold",
+    marginTop: 50,
+    marginBottom: 15,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+});
