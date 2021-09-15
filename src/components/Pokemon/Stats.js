80b5a86329aa14f40b4ba4328cd8ad83822c01d9
Agustin Navarro Galdon <xAgustin93@gmail.com>
2021-09-15 22:23:27 +0200

    Pintando stats del pokemon

---
+++ src/components/Pokemon/Stats.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { map, capitalize } from "lodash";
+
+export default function Stats(props) {
+  const { stats } = props;
+
+  const barStyles = (num) => {
+    const color = num > 49 ? "#00ac17" : "#ff3e3e";
+    return {
+      backgroundColor: color,
+      width: `${num}%`,
+    };
+  };
+
+  return (
+    <View style={styles.content}>
+      <Text style={styles.title}>Base Stats</Text>
+      {map(stats, (item, index) => (
+        <View key={index} style={styles.block}>
+          <View style={styles.blockTitle}>
+            <Text style={styles.statName}>{capitalize(item.stat.name)}</Text>
+          </View>
+          <View style={styles.blockInfo}>
+            <Text style={styles.number}>{item.base_stat}</Text>
+            <View style={styles.bgBar}>
+              <View style={[styles.bar, barStyles(item.base_stat)]} />
+            </View>
+          </View>
+        </View>
+      ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  content: {
+    paddingHorizontal: 20,
+    marginTop: 40,
+    marginBottom: 80,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+    paddingBottom: 5,
+  },
+  block: {
+    flexDirection: "row",
+    paddingVertical: 5,
+  },
+  blockTitle: {
+    width: "30%",
+  },
+  statName: {
+    fontSize: 12,
+    color: "#6b6b6b",
+  },
+  blockInfo: {
+    width: "70%",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  number: {
+    width: "12%",
+    fontSize: 12,
+  },
+  bgBar: {
+    backgroundColor: "#dedede",
+    width: "88%",
+    height: 5,
+    borderRadius: 20,
+    overflow: "hidden",
+  },
+  bar: {
+    // backgroundColor: "red",
+    // width: "40%",
+    height: 5,
+    borderRadius: 20,
+  },
+});
--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -3,6 +3,7 @@ import { ScrollView } from "react-native";
 import { getPokemonDetailsApi } from "../api/pokemon";
 import Header from "../components/Pokemon/Header";
 import Type from "../components/Pokemon/Type";
+import Stats from "../components/Pokemon/Stats";
 
 export default function Pokemon(props) {
   const {
@@ -33,6 +34,7 @@ export default function Pokemon(props) {
         type={pokemon.types[0].type.name}
       />
       <Type types={pokemon.types} />
+      <Stats stats={pokemon.stats} />
     </ScrollView>
   );
 }
