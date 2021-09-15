49ac3c71fb3690760dc1ac7bc9282c290c09d06f
Agustin Navarro Galdon <xAgustin93@gmail.com>
2021-09-15 22:32:27 +0200

    Modificando el header de la nacegacion del pokemon

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { ScrollView } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
 import { getPokemonDetailsApi } from "../api/pokemon";
 import Header from "../components/Pokemon/Header";
 import Type from "../components/Pokemon/Type";
@@ -11,6 +12,21 @@ export default function Pokemon(props) {
     route: { params },
   } = props;
   const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => null,
+      headerLeft: () => (
+        <Icon
+          name="arrow-left"
+          color="#fff"
+          size={20}
+          style={{ marginLeft: 20 }}
+          onPress={navigation.goBack}
+        />
+      ),
+    });
+  }, [navigation, params]);
 
   useEffect(() => {
     (async () => {
