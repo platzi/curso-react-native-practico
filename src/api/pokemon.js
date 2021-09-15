c6cd8f90b8ca7e38b302bc6d47f4b94989c598d8
Agustin Navarro Galdon <xAgustin93@gmail.com>
2021-09-15 21:25:34 +0200

    Obtener informacion del pokemon

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -20,3 +20,14 @@ export async function getPokemonDetailsByUrlApi(url) {
     throw error;
   }
 }
+
+export async function getPokemonDetailsApi(id) {
+  try {
+    const url = `${API_HOST}/pokemon/${id}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+import { getPokemonDetailsApi } from "../api/pokemon";
 
 export default function Pokemon(props) {
-  const { navigation, route } = props;
-  console.log(route);
+  const {
+    navigation,
+    route: { params },
+  } = props;
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getPokemonDetailsApi(params.id);
+        setPokemon(response);
+      } catch (error) {
+        navigation.goBack();
+      }
+    })();
+  }, [params]);
+
+  if (!pokemon) return null;
 
   return (
     <View>
       <Text>Estamos en un POKEMON</Text>
+      <Text>{pokemon.name}</Text>
     </View>
   );
 }
