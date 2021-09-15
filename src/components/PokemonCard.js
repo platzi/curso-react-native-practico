a33cb6c31dba575ec99295d101a7a5c45a5ddec4
Agustin Navarro Galdon <xAgustin93@gmail.com>
2021-09-15 21:16:20 +0200

    Navegando a la screen del pokemon

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -7,17 +7,18 @@ import {
   TouchableWithoutFeedback,
 } from "react-native";
 import { capitalize } from "lodash";
+import { useNavigation } from "@react-navigation/native";
 import getColorByPokemonType from "../utils/getColorByPokemonType";
 
 export default function PokemonCard(props) {
   const { pokemon } = props;
+  const navigation = useNavigation();
 
   const pokemonColor = getColorByPokemonType(pokemon.type);
   const bgStyles = { backgroundColor: pokemonColor, ...styles.bgStyles };
 
   const goToPokemon = () => {
-    console.log(`Vamos al pokemon: ${pokemon.name}`);
-    console.log(pokemon);
+    navigation.navigate("Pokemon", { id: pokemon.id });
   };
 
   return (
--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function Pokemon() {
+export default function Pokemon(props) {
+  const { navigation, route } = props;
+  console.log(route);
+
   return (
     <View>
       <Text>Estamos en un POKEMON</Text>
