988e512a3e7ecfd9b800020142a92a0d7d9d6917
Agustin Navarro Galdon <xAgustin93@gmail.com>
2021-09-16 19:40:40 +0200

    Solucion al problema de navegación

--- src/navigation/FavoriteNavigation.js
+++ src/navigation/FavoriteNavigation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import FavoriteScreen from "../screens/Favorite";
+import PokemonScreen from "../screens/Pokemon";
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,14 @@ export default function FavoriteNavigation() {
         name="Favorite"
         component={FavoriteScreen}
         options={{ title: "Favoritos" }}
+      />
+      <Stack.Screen
+        name="Pokemon"
+        component={PokemonScreen}
+        options={{
+          title: "",
+          headerTransparent: true,
+        }}
       />
     </Stack.Navigator>
   );
